a834d8d2425f9597ff30ee04765c75c8634dc5e1
Phil <philwsun@gmail.com>
2016-12-14 13:58:09 -0500

    Use double instead of ld; multithread

--- aSquare.cpp
+++ aSquare.cpp
@@ -4,10 +4,22 @@
 #include <cmath>
 #include <thread>
 using namespace std;
-typedef long double ld;
+typedef double ld;
 
 ld *memo1, *memo2, *res;
 int kt, ktp, ktp2, nt, ntp;
+const int NUMT = 8;
+
+void calcOverA(const int n, const int tnum) {
+	const int rn = nt - n;
+	const int bot = tnum * (rn / NUMT);
+	const int top = (tnum == NUMT - 1) ? rn+1 : (tnum + 1) * (rn / NUMT);
+	for (int a = bot; a < top; a++)
+		for (int b = 0; b <= nt - n - a; b++) {
+				memo2[a*ntp + b] = (a+1.0)/(a+b+2.0) * (1.0 + memo1[(a+1)*ntp + b]) + 
+								   (b+1.0)/(a+b+2.0) * max(memo1[0], memo1[a*ntp + b+1]);
+			}
+}
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) kt = 50, nt = 50;
@@ -20,17 +32,13 @@ int main(int argc, char *argv[]) {
 	memo1	= new ld[ktp2];
 	memo2	= new ld[ktp2];
 	memset(memo1, 0, ktp2*sizeof(ld));
+	thread ts[NUMT];
 	for (int n = 1; n < ntp; n++) {
-		for (int a = 0; a <= nt - n; a++)
-			for (int b = 0; b <= nt - n - a; b++) {
-				memo2[a*ntp + b] = (a+1.0L)/(a+b+2.0L) * (1.0L + memo1[(a+1)*ntp + b]) + 
-								   (b+1.0L)/(a+b+2.0L) * max(memo1[0], memo1[a*ntp + b+1]);
-			}
-		ld* tmp = memo1;
-		memo1 = memo2;
-		memo2 = tmp;
+		for (int i = 0; i < NUMT; i++) ts[i] = thread(calcOverA, n, i);
+		for (int i = 0; i < NUMT; i++) ts[i].join();
+		swap(memo1, memo2);
 		res[n] = memo1[0];
-		cout << n << ' ' << res[n] << '\n';
+		cout << n << ' ' << res[n] << '\n'; 
 	}
 
 	if (argc == 1) {
